192424378e0a49f13ac1b6f88537a442a5922b43
xfrr <francisco.romero.1994@gmail.com>
2025-09-27 00:05:04 +0200

    feat: add domain WithEventTimestamp function to set event timestamp

--- domain/event_options.go
+++ domain/event_options.go
@@ -1,3 +1,12 @@
 package domain
 
+import "time"
+
 type EventOption func(*BaseEvent)
+
+// WithEventTimestamp sets the timestamp of the event.
+func WithEventTimestamp(t time.Time) EventOption {
+	return func(e *BaseEvent) {
+		e.timestamp = t
+	}
+}
